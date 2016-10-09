afb709c35f05a022bcda606f818f1ff04ed182a8
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-10-09 14:08:00 -0400

    pause scraping until further instructions

--- scripts/scrape_content.py
+++ scripts/scrape_content.py
@@ -59,5 +59,5 @@ if __name__ == '__main__':
 
     from pprint import pprint
 
-    with open('test2.html', 'r') as dummy_html:
+    with open('test.html', 'r') as dummy_html:
         pprint(scrape(dummy_html))
