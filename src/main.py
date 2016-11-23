62b5201e29e8d272eee0a442a8b8a8c8cc86068d
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-11-22 22:51:30 -0500

    implemented merging duplicate row algorithm, wrap up version 1

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from os import walk
 from shutil import rmtree
 from time import time
 
-from clean_db import download
+from clean_db import CleanedData
 from settings import HTML_DIR, SAVE_PROG
 from miner import export
 from spider import save_response
@@ -44,7 +44,11 @@ if __name__ == '__main__':
 
     print "Total mining runtime: {0:.3f} s".format((end - start))
 
-    download(output)
+    df_obj = CleanedData(output)
+
+    df_obj.clean_addr()
+    df_obj.merge_nulls()
+    df_obj.download("2015_clean.csv")
 
     with open(SAVE_PROG, 'w') as checkpoint:
         checkpoint.write(sorted(file_array)[-1][:-5])
