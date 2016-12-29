b256442e2435c899930e286ddc62731ef5716e51
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2016-12-29 10:46:52 -0500

    Update settings.py

--- close_crawl/modules/settings.py
+++ close_crawl/modules/settings.py
@@ -12,7 +12,7 @@ from os import path
 # browser settings
 HEADER = ("Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1)"
           " Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1")
-URL = "http://casesearch.courts.state.md.us/casesearch//inquiry-index.jsp"
+URL = "http://casesearch.courts.state.md.us/casesearch/"
 CASE_PAT = "24{type}{year}00{num}"
 
 # temporary directory settings
