0d7abcb1caa258ede2cba73c9e131301cec19ca4
Sabbir Ahmed <sabbir0ahmed0@gmail.com>
2017-01-05 18:00:54 -0500

    add Linux dist

--- close_crawl/frontend/config.py
+++ close_crawl/frontend/config.py
@@ -4,9 +4,18 @@ from random import SystemRandom, uniform
 
 from flask import Flask
 
+# extra modules to manually add hooks for building executible
+# TODO: fix this
+from .extra_mods import *  # pylint: disable=unused-import
+
 BASE_DIR = path.dirname(path.abspath(__file__))
 TEMPLATE_DIR = path.join(BASE_DIR, "templates")
 STATIC_DIR = path.join(BASE_DIR, "static")
+
+# temporary hardcoded dir
+# TODO: figure out dynamic path for PyInstaller build
+TEMPLATE_DIR = "/home/sabbir/Desktop/close-crawl/close_crawl/frontend/templates"
+STATIC_DIR = "/home/sabbir/Desktop/close-crawl/close_crawl/frontend/static"
 
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 
